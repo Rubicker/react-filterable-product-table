1087813fc6611956135d14ff3d9c549e13222f39
Rubicker <newsl00001@gmail.com>
2017-12-26 18:15:13 +0800

    :white_check_mark: Add FilterableProductTable Tests

--- src/components/FilterableProductTable/index.spec.js
+++ src/components/FilterableProductTable/index.spec.js
@@ -30,3 +30,15 @@ it('should initialize the filterText state to empty string', () => {
 it('should initialize the inStockOnly state to false', () => {
   expect(wrapper).to.have.state('inStockOnly').to.equal(false)
 })
+
+// check handleFilterTextInput & handleInStockINput functions
+it('should update the state filter to the correct value', () => {
+  const filterText = faker.lorem.word()
+  wrapper.instance().handleFilterTextInput(filterText)
+  expect(wrapper).to.have.state('filterText').to.equal(filterText)
+})
+
+it('should update the state inStockOnly to true', () => {
+  wrapper.instance().handleInStockInput(true)
+  expect(wrapper).to.have.state('inStockOnly').to.equal(true)
+})
