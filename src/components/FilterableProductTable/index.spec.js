a21828ce9a9af995811a9923753c3e955d7c41b3
Rubicker <newsl00001@gmail.com>
2017-12-27 09:41:21 +0800

    :white_check_mark: Add SearchBar test

--- src/components/FilterableProductTable/index.spec.js
+++ src/components/FilterableProductTable/index.spec.js
@@ -20,25 +20,25 @@ describe('FilterableProductTable', () => {
       />
     )
   })
-})
 
-// check if the initial state is empty string
-it('should initialize the filterText state to empty string', () => {
-  expect(wrapper).to.have.state('filterText').to.equal('')
-})
+  // check if the initial state is empty string
+  it('should initialize the filterText state to empty string', () => {
+    expect(wrapper).to.have.state('filterText').to.equal('')
+  })
 
-it('should initialize the inStockOnly state to false', () => {
-  expect(wrapper).to.have.state('inStockOnly').to.equal(false)
-})
+  it('should initialize the inStockOnly state to false', () => {
+    expect(wrapper).to.have.state('inStockOnly').to.equal(false)
+  })
 
-// check handleFilterTextInput & handleInStockINput functions
-it('should update the state filter to the correct value', () => {
-  const filterText = faker.lorem.word()
-  wrapper.instance().handleFilterTextInput(filterText)
-  expect(wrapper).to.have.state('filterText').to.equal(filterText)
-})
+  // check handleFilterTextInput & handleInStockINput functions
+  it('should update the state filter to the correct value', () => {
+    const filterText = faker.lorem.word()
+    wrapper.instance().handleFilterTextInput(filterText)
+    expect(wrapper).to.have.state('filterText').to.equal(filterText)
+  })
 
-it('should update the state inStockOnly to true', () => {
-  wrapper.instance().handleInStockInput(true)
-  expect(wrapper).to.have.state('inStockOnly').to.equal(true)
+  it('should update the state inStockOnly to true', () => {
+    wrapper.instance().handleInStockInput(true)
+    expect(wrapper).to.have.state('inStockOnly').to.equal(true)
+  })
 })
